cc0f697473e16fc686c48fd0287d586dc32df8e9
FilipeMagal <filipi99ff@gmail.com>
2025-04-29 02:50:55 -0300

    notification success

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,6 +43,7 @@ dependencies {
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     implementation ("com.google.android.material:material:1.11.0")
     implementation(libs.viewpager2)
+    implementation("com.google.firebase:firebase-messaging:23.4.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
