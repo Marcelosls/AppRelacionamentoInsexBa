63630f0f97205361712acf5573e26d4442092f7e
Marcelo Leal <marcelo090701@gmail.com>
2025-05-03 12:50:31 -0300

    alteração das cores e logo e placeholder do nome do app

--- app/src/main/java/com/br/apprelacionamento/activities/InicioActivity.java
+++ app/src/main/java/com/br/apprelacionamento/activities/InicioActivity.java
@@ -1,9 +1,14 @@
 package com.br.apprelacionamento.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +23,29 @@ public class InicioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        TextView textView = findViewById(R.id.txtNomeApp);
+
+        textView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                Shader textShader = new LinearGradient(
+                        0, 0, textView.getWidth(), 0,  // Horizontal: x0, y0, x1, y1
+                        new int[]{
+                                Color.parseColor("#D2B162"), // início
+                                Color.parseColor("#A67C00"), // meio (tom mais escuro)
+                                Color.parseColor("#D2B162")  // fim
+                        },
+                        new float[]{0f, 0.5f, 1f}, // Posições relativas das cores
+                        Shader.TileMode.CLAMP
+                );
+
+                textView.getPaint().setShader(textShader);
+                textView.invalidate(); // Redesenha com o shader
+            }
+        });
 
         btnLogin = findViewById(R.id.btnLogin);
         btnCadastro = findViewById(R.id.btnCadastro);
